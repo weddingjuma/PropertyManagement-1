6635522afee4e07edbb9a5ab66a08d964117df29
Kelvin <kgraddick@wavelinkllc.com>
2017-05-27 04:47:27 -0400

    Settings page updates

--- PropertyManagement/Controllers/SettingsController.cs
+++ PropertyManagement/Controllers/SettingsController.cs
@@ -1,13 +1,128 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Acr.UserDialogs;
+using PropertyManagement.Components;
+using PropertyManagement.Models;
 using PropertyManagement.Pages;
+using Xamarin.Forms;
 
 namespace PropertyManagement.Controllers
 {
 	public class SettingsController : BaseController<SettingsPage>
 	{
-		public SettingsController()
+		public Dictionary<string, int> Properties { get; set; } = new Dictionary<string, int>
+		{
+			{ "The Palms Apartments", 1 }, { "LullWater Apartments", 2 }, { "Suger Mill Luxury Apartments", 3 }
+		};
+
+		public Dictionary<string, int> LeaseMonths { get; set; } = new Dictionary<string, int>
+		{
+			{ "6 Months", 6 }, { "9 Months", 9 }, { "12 Months", 12 }
+		};
+
+		private bool IsValid { get; set; }
+		private string ErrorMessage { get; set; }
+
+		public SettingsController() : base()
 		{
 			Page = new SettingsPage(this);
+		}
+
+		public void OnSaveButtonTapped(object sender, EventArgs e)
+		{
+			SetValidationState();
+
+			if (IsValid)
+			{
+				Task.Run(async () =>
+				{
+					UserDialogs.Instance.ShowLoading("Saving..", MaskType.Gradient);
+
+					ApplicationContext.User.EmailAddress = Page.EmailAddressEntry.Text;
+					ApplicationContext.User.PhoneNumber = Page.PhoneNumberEntry.Text;
+					ApplicationContext.User.Password = Page.PasswordEntry.Text;
+					ApplicationContext.User.FirstName = Page.FirstNameEntry.Text;
+					ApplicationContext.User.LastName = Page.LastNameEntry.Text;
+					ApplicationContext.User.PropertyId = Properties[Page.PropertyPicker.Items[Page.PropertyPicker.SelectedIndex]];
+					ApplicationContext.User.Unit = Page.UnitEntry.Text;
+					ApplicationContext.User.LeaseMonths = LeaseMonths[Page.LeaseMonthsPicker.Items[Page.LeaseMonthsPicker.SelectedIndex]];
+
+					var response = await UserActions.Save();
+					if (response.Success)
+					{
+						ApplicationContext.InitializeControllers();
+						Device.BeginInvokeOnMainThread(() => Application.Current.MainPage = ApplicationContext.MainController.Page);
+					}
+					else
+					{
+						Device.BeginInvokeOnMainThread(() => Page.DisplayAlert(string.Empty, response.ErrorMessage, "OK"));
+					}
+					UserDialogs.Instance.HideLoading();
+				});
+			}
+			else
+			{
+				Page.DisplayAlert(string.Empty, ErrorMessage, "OK");
+			}
+		}
+
+		private void SetValidationState()
+		{
+			ErrorMessage = string.Empty;
+
+			if (string.IsNullOrWhiteSpace(Page.EmailAddressEntry.Text) && string.IsNullOrWhiteSpace(Page.PhoneNumberEntry.Text))
+			{
+				ErrorMessage = "Either an email address or phone number must be entered. Please try again.\n";
+			}
+
+			if (!string.IsNullOrWhiteSpace(Page.EmailAddressEntry.Text) && !Regex.IsMatch(Page.EmailAddressEntry.Text, @"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z", RegexOptions.IgnoreCase))
+			{
+				ErrorMessage += "The entered email address was invalid. Please try again.\n";
+			}
+
+			if (!string.IsNullOrWhiteSpace(Page.PhoneNumberEntry.Text) && !Regex.Match(Page.PhoneNumberEntry.Text, @"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$").Success)
+			{
+				ErrorMessage += "The entered phone number was invalid. Please try again.\n";
+			}
+
+			if (string.IsNullOrWhiteSpace(Page.PasswordEntry.Text) || Page.PasswordEntry.Text.Length < 8 || Page.PasswordEntry.Text.Length > 20)
+			{
+				ErrorMessage += "You must enter a password between 8 and 20 characters.\n";
+			}
+
+			if (string.IsNullOrWhiteSpace(Page.FirstNameEntry.Text) || Page.FirstNameEntry.Text.Length < 2 || Page.FirstNameEntry.Text.Length > 20)
+			{
+				ErrorMessage += "You must enter a first name between 2 and 20 characters.\n";
+			}
+
+			if (string.IsNullOrWhiteSpace(Page.LastNameEntry.Text) || Page.LastNameEntry.Text.Length < 2 || Page.LastNameEntry.Text.Length > 20)
+			{
+				ErrorMessage += "You must enter a last name between 2 and 20 characters.\n";
+			}
+
+			if (string.IsNullOrWhiteSpace(Page.UnitEntry.Text))
+			{
+				ErrorMessage += "You must enter a unit number.\n";
+			}
+
+			if (string.IsNullOrWhiteSpace(Page.RentEntry.Text))
+			{
+				ErrorMessage += "You must enter a rent amount.\n";
+			}
+
+			if (Page.PropertyPicker.SelectedItem == null)
+			{
+				ErrorMessage += "You must select a property.\n";
+			}
+
+			if (Page.LeaseMonthsPicker.SelectedItem == null)
+			{
+				ErrorMessage += "You must select a lease term.\n";
+			}
+
+			IsValid = string.IsNullOrWhiteSpace(ErrorMessage);
 		}
 	}
 }
--- PropertyManagement/Pages/SettingsPage.cs
+++ PropertyManagement/Pages/SettingsPage.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using PropertyManagement.Components;
 using PropertyManagement.Controllers;
 using PropertyManagement.Renderers;
@@ -9,6 +10,18 @@ namespace PropertyManagement.Pages
 {
 	public class SettingsPage : BasePage<SettingsController>
 	{
+		public Entry EmailAddressEntry { get; set; }
+		public Entry PhoneNumberEntry { get; set; }
+		public Entry PasswordEntry { get; set; }
+		public Entry PasswordConfirmEntry { get; set; }
+		public Entry FirstNameEntry { get; set; }
+		public Entry LastNameEntry { get; set; }
+		public Entry UnitEntry { get; set; }
+		public Entry RentEntry { get; set; }
+		public Picker PropertyPicker { get; set; }
+		public Picker LeaseMonthsPicker { get; set; }
+		public DatePicker DueDatePicker { get; set; }
+
 		public SettingsPage(SettingsController controller) : base(controller)
 		{
 			Title = "Settings";
@@ -19,12 +32,301 @@ namespace PropertyManagement.Pages
 
 			var titleView = new TitleView(Title);
 
+			FirstNameEntry = new ExtendedEntry
+			{
+				HeightRequest = 50,
+				BackgroundColor = Color.Transparent,
+				FontFamily = ApplicationSettings.RegularFontFamily,
+				FontSize = 15,
+				TextColor = Color.FromHex("7f8c8d"),
+				PlaceholderColor = Color.FromHex("bdc3c7"),
+				Placeholder = "First Name",
+				Text = ApplicationContext.User.FirstName
+			};
+
+			var entry1Divider = new BoxView
+			{
+				BackgroundColor = Color.FromHex("f5f5f5"),
+				HorizontalOptions = LayoutOptions.FillAndExpand,
+				HeightRequest = 1
+			};
+
+			LastNameEntry = new ExtendedEntry
+			{
+				HeightRequest = 50,
+				BackgroundColor = Color.Transparent,
+				FontFamily = ApplicationSettings.RegularFontFamily,
+				FontSize = 15,
+				TextColor = Color.FromHex("7f8c8d"),
+				PlaceholderColor = Color.FromHex("bdc3c7"),
+				Placeholder = "Last Name",
+				Text = ApplicationContext.User.LastName
+			};
+
+			var entry2Divider = new BoxView
+			{
+				BackgroundColor = Color.FromHex("f5f5f5"),
+				HorizontalOptions = LayoutOptions.FillAndExpand,
+				HeightRequest = 1
+			};
+
+			EmailAddressEntry = new ExtendedEntry
+			{
+				HeightRequest = 50,
+				BackgroundColor = Color.Transparent,
+				FontFamily = ApplicationSettings.RegularFontFamily,
+				FontSize = 15,
+				TextColor = Color.FromHex("7f8c8d"),
+				PlaceholderColor = Color.FromHex("bdc3c7"),
+				Placeholder = "Email",
+				Text = ApplicationContext.User.EmailAddress
+			};
+
+			var entry3Divider = new BoxView
+			{
+				BackgroundColor = Color.FromHex("f5f5f5"),
+				HorizontalOptions = LayoutOptions.FillAndExpand,
+				HeightRequest = 1
+			};
+
+			PhoneNumberEntry = new ExtendedEntry
+			{
+				HeightRequest = 50,
+				BackgroundColor = Color.Transparent,
+				FontFamily = ApplicationSettings.RegularFontFamily,
+				FontSize = 15,
+				TextColor = Color.FromHex("7f8c8d"),
+				PlaceholderColor = Color.FromHex("bdc3c7"),
+				Placeholder = "Phone Number",
+				Text = ApplicationContext.User.PhoneNumber
+			};
+
+			var entry4Divider = new BoxView
+			{
+				BackgroundColor = Color.FromHex("f5f5f5"),
+				HorizontalOptions = LayoutOptions.FillAndExpand,
+				HeightRequest = 1
+			};
+
+			PasswordEntry = new ExtendedEntry
+			{
+				IsPassword = true,
+				HeightRequest = 50,
+				BackgroundColor = Color.Transparent,
+				FontFamily = ApplicationSettings.RegularFontFamily,
+				FontSize = 15,
+				TextColor = Color.FromHex("7f8c8d"),
+				PlaceholderColor = Color.FromHex("bdc3c7"),
+				Placeholder = "Password",
+				Text = ApplicationContext.User.Password
+			};
+
+			var entry5Divider = new BoxView
+			{
+				BackgroundColor = Color.FromHex("f5f5f5"),
+				HorizontalOptions = LayoutOptions.FillAndExpand,
+				HeightRequest = 1
+			};
+
+			PasswordConfirmEntry = new ExtendedEntry
+			{
+				IsPassword = true,
+				HeightRequest = 50,
+				BackgroundColor = Color.Transparent,
+				FontFamily = ApplicationSettings.RegularFontFamily,
+				FontSize = 15,
+				TextColor = Color.FromHex("7f8c8d"),
+				PlaceholderColor = Color.FromHex("bdc3c7"),
+				Placeholder = "Password (confirm)"
+			};
+
+			var entry6Divider = new BoxView
+			{
+				BackgroundColor = Color.FromHex("f5f5f5"),
+				HorizontalOptions = LayoutOptions.FillAndExpand,
+				HeightRequest = 1
+			};
+
+			UnitEntry = new ExtendedEntry
+			{
+				HeightRequest = 50,
+				BackgroundColor = Color.Transparent,
+				FontFamily = ApplicationSettings.RegularFontFamily,
+				FontSize = 15,
+				TextColor = Color.FromHex("7f8c8d"),
+				PlaceholderColor = Color.FromHex("bdc3c7"),
+				Placeholder = "Unit Number",
+				Text = ApplicationContext.User.Unit
+			};
+
+			var entry7Divider = new BoxView
+			{
+				BackgroundColor = Color.FromHex("f5f5f5"),
+				HorizontalOptions = LayoutOptions.FillAndExpand,
+				HeightRequest = 1
+			};
+
+			RentEntry = new ExtendedEntry
+			{
+				HeightRequest = 50,
+				BackgroundColor = Color.Transparent,
+				FontFamily = ApplicationSettings.RegularFontFamily,
+				FontSize = 15,
+				TextColor = Color.FromHex("7f8c8d"),
+				PlaceholderColor = Color.FromHex("bdc3c7"),
+				Placeholder = "Rent Amount"
+			};
+
+			PropertyPicker = new Picker
+			{
+				//HeightRequest = 50,
+				BackgroundColor = Color.Transparent,
+				TextColor = Color.FromHex("7f8c8d"),
+				Title = "Select Property Name",
+				VerticalOptions = LayoutOptions.CenterAndExpand
+			};
+
+			foreach (string property in controller.Properties.Keys)
+			{
+				PropertyPicker.Items.Add(property);
+			}
+
+			//PropertyPicker.
+
+			LeaseMonthsPicker = new Picker
+			{
+				//HeightRequest = 50,
+				BackgroundColor = Color.Transparent,
+				TextColor = Color.FromHex("7f8c8d"),
+				Title = " Select Lease Term",
+				VerticalOptions = LayoutOptions.CenterAndExpand
+			};
+
+			foreach (string leaseMonth in controller.LeaseMonths.Keys)
+			{
+				LeaseMonthsPicker.Items.Add(leaseMonth);
+			}
+
+			//if (controller.LeaseMonths.ContainsValue(ApplicationContext.User.LeaseMonths))
+			//{
+			//	LeaseMonthsPicker.SelectedIndex[controller.LeaseMonths.FirstOrDefault(x => x.Value == ApplicationContext.User.LeaseMonths).Key]
+			//}
+
+			DueDatePicker = new DatePicker
+			{
+				Format = "D",
+				BackgroundColor = Color.Transparent,
+				TextColor = Color.FromHex("7f8c8d"),
+				VerticalOptions = LayoutOptions.CenterAndExpand
+			};
+
+			var entryLayout = new StackLayout
+			{
+				Children = { FirstNameEntry, entry1Divider, LastNameEntry, entry2Divider, EmailAddressEntry, entry3Divider,
+					PhoneNumberEntry, entry4Divider, PasswordEntry, entry5Divider, PasswordConfirmEntry, entry6Divider,
+					UnitEntry, entry7Divider, RentEntry, PropertyPicker, LeaseMonthsPicker, DueDatePicker },
+				HorizontalOptions = LayoutOptions.FillAndExpand,
+				Spacing = 0,
+				Padding = new Thickness(20, 20, 20, 20)
+
+			};
+
+			var credentialsBackgroundFrame = new ExtendedFrame
+			{
+				Content = entryLayout,
+				OutlineColor = Color.White,
+				ActualBackgroundColor = Color.White,
+				HasShadow = false,
+				CornerRadius = 2,
+				BorderWidth = 0,
+				VerticalOptions = LayoutOptions.FillAndExpand,
+				HorizontalOptions = LayoutOptions.FillAndExpand,
+				Padding = 0
+			};
+
+			var credentialsIcon = new Image
+			{
+				Source = ImageSource.FromFile("cloud_key_white"),
+				Aspect = Aspect.AspectFit,
+				HeightRequest = 50,
+				WidthRequest = 50
+			};
+
+			var credentialsIconFrame = new ExtendedFrame
+			{
+				Content = credentialsIcon,
+				OutlineColor = Color.Orange,
+				ActualBackgroundColor = Color.Orange,
+				HasShadow = false,
+				CornerRadius = 30,
+				BorderWidth = 0,
+				HeightRequest = 70,
+				WidthRequest = 70,
+				ShadowColor = Color.Black,
+				ShadowOpacity = 0.3,
+				ShadowBlurRadius = 1,
+				ShadowOffsetX = 0,
+				ShadowOffsetY = 0,
+			};
+
+			var credentialsLayout = new RelativeLayout
+			{
+				HorizontalOptions = LayoutOptions.FillAndExpand,
+				HeightRequest = 168
+			};
+
+			credentialsLayout.Children.Add(
+				credentialsBackgroundFrame,
+				Constraint.Constant(25),
+				Constraint.Constant(30),
+				Constraint.RelativeToParent(parent => { return parent.Width - 50; }),
+				Constraint.Constant(660)
+			);
+
+			credentialsLayout.Children.Add(
+				credentialsIconFrame,
+				Constraint.RelativeToParent(parent => { return parent.Width / 2 - 30; }),
+				Constraint.Constant(0),
+				Constraint.Constant(60),
+				Constraint.Constant(60)
+			);
+
+			var saveButton = new Button
+			{
+				HorizontalOptions = LayoutOptions.FillAndExpand,
+				VerticalOptions = LayoutOptions.Start,
+				HeightRequest = 55,
+				BorderRadius = 0,
+				FontFamily = ApplicationSettings.BoldFontFamily,
+				FontSize = 17,
+				TextColor = Color.White,
+				BackgroundColor = Color.Black.MultiplyAlpha(0.2),
+				Text = "Save"
+			};
+			saveButton.Clicked += Controller.OnSaveButtonTapped;
+
 			Content = new StackLayout
 			{
-				Children = { titleView },
+				Children = 
+				{ 
+					titleView,
+					new ScrollView 
+					{
+						Content = new StackLayout
+						{
+							Children = { credentialsLayout, saveButton },
+							HorizontalOptions = LayoutOptions.FillAndExpand,
+							HeightRequest = 800,
+							Spacing = 10
+						},
+						HorizontalOptions = LayoutOptions.FillAndExpand,
+						VerticalOptions = LayoutOptions.FillAndExpand
+					}
+				},
 				HorizontalOptions = LayoutOptions.FillAndExpand,
 				VerticalOptions = LayoutOptions.FillAndExpand,
-				Spacing = 0
+				Spacing = 10,
+				Padding = 0
 			};
 		}
 	}
